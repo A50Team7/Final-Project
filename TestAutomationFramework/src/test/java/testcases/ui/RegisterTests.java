f1154e578ee91c4d6bc4f404cf15ed87c4d1e96d
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-10 15:35:00 +0300

    Added a cleanup for the RegisterTests

--- TestAutomationFramework/src/test/java/testcases/ui/RegisterTests.java
+++ TestAutomationFramework/src/test/java/testcases/ui/RegisterTests.java
@@ -1,8 +1,10 @@
 package testcases.ui;
 
 import com.testframework.Utils;
+import com.testframework.api.UserControllerHelper;
 import com.testframework.factories.UserFactory;
 import com.testframework.models.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.RegisterPage;
@@ -22,6 +24,11 @@ public class RegisterTests extends BaseTest {
     public void registerWithValidData() {
         registerPage.enterAllDataAndRegister(user);
         registerPage.assertWelcomeMessagePresent();
+        // To Do: assertion using REST-Assured request - confirmation that the user is, in fact, in the database
     }
 
+    @AfterEach
+    public void userCleanup() {
+        UserControllerHelper.deleteUser("username", String.format("'%s'", user.getUsername()));
+    }
 }
