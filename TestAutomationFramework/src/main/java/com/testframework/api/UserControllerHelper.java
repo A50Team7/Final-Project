37e3d5e01a6498b0d03213875cf2cbbbf7837826
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-10 15:07:24 +0300

    Added a method to delete users
    The method connects directly to the database; url, username, and password for the database are in the config file; added 2 dependencies in the pom file

---
+++ TestAutomationFramework/src/main/java/com/testframework/api/UserControllerHelper.java
@@ -0,0 +1,31 @@
+package com.testframework.api;
+
+import com.testframework.Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserControllerHelper {
+    public static void deleteUser(String key, String value) {
+        String dbUrl = Utils.getConfigPropertyByKey("weare.db.url");
+        String username = Utils.getConfigPropertyByKey("weare.db.username");
+        String password = Utils.getConfigPropertyByKey("weare.db.password");
+        String query = String.format("DELETE FROM users WHERE %s='%s';", key, value);
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(Connection con = DriverManager.getConnection(dbUrl, username, password)) {
+            try (Statement stmt = con.createStatement()) {
+                stmt.executeUpdate(query);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
