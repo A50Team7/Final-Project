88a670f674560200c703829211c4ee4df2e06196
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-11 14:22:19 +0300

    Updated the test registerWithValidData and Added a new test registerWithWhiteSpaceInUsernameField
    The update in the first test is an assertion that the user, in fact, exists in the database.
    The new test enters valid data but only the username is empty, it then asserts that an error message is displayed and that in the database no user was created.

--- TestAutomationFramework/src/test/java/testcases/ui/RegisterTests.java
+++ TestAutomationFramework/src/test/java/testcases/ui/RegisterTests.java
@@ -5,9 +5,12 @@ import com.testframework.api.UserControllerHelper;
 import com.testframework.factories.UserFactory;
 import com.testframework.models.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.RegisterPage;
+
+import java.sql.ResultSet;
 
 public class RegisterTests extends BaseTest {
     private static final String registerPageUrl = Utils.getConfigPropertyByKey("weare.register.url");
@@ -21,10 +24,28 @@ public class RegisterTests extends BaseTest {
     }
 
     @Test
-    public void registerWithValidData() {
+    public void registerWithValidData_Should_Pass() {
         registerPage.enterAllDataAndRegister(user);
+
         registerPage.assertWelcomeMessagePresent();
-        // To Do: assertion using REST-Assured request - confirmation that the user is, in fact, in the database
+
+        ResultSet resultSet = UserControllerHelper.getUser("username", String.format("'%s'", user.getUsername()));
+
+        Assertions.assertTrue(UserControllerHelper.userExists(resultSet),
+                "The user is not found in the database.");
+    }
+
+    @Test
+    public void registerWithWhiteSpaceInUsernameField_Should_Fail() {
+        user.setUsername(" ");
+        registerPage.enterAllDataAndRegister(user);
+
+        registerPage.assertErrorMessagePresent("no whitespaces");
+
+        ResultSet resultSet = UserControllerHelper.getUser("username", String.format("'%s'", user.getUsername()));
+
+        Assertions.assertFalse(UserControllerHelper.userExists(resultSet),
+                "The user is created in the database, when it shouldn't.");
     }
 
     @AfterEach
