5035c0fab3ce67c1e15a0d57c62108d686adcac0
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-11 12:46:38 +0300

    Updated the logic behind UserControllerHelper.deleteUser()
    Increased abstraction; Added validation before the delete query is sent and after

--- TestAutomationFramework/src/main/java/com/testframework/api/UserControllerHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/api/UserControllerHelper.java
@@ -2,17 +2,18 @@ package com.testframework.api;
 
 import com.testframework.Utils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UserControllerHelper {
-    public static void deleteUser(String key, String value) {
-        String dbUrl = Utils.getConfigPropertyByKey("weare.db.url");
-        String username = Utils.getConfigPropertyByKey("weare.db.username");
-        String password = Utils.getConfigPropertyByKey("weare.db.password");
-        String query = String.format("DELETE FROM users WHERE %s=%s;", key, value);
+    private static final String dbUrl = Utils.getConfigPropertyByKey("weare.db.url");
+    private static final String username = Utils.getConfigPropertyByKey("weare.db.username");
+    private static final String password = Utils.getConfigPropertyByKey("weare.db.password");
+    private static final String deleteQuery =  "DELETE FROM users WHERE %s=%s;";
+    private static final String findQuery = "SELECT * FROM users WHERE %s=%s;";
+
+
+    private static void delete(String key, String value) {
+        String query = String.format(deleteQuery, key, value);
 
         try {
             Class.forName("org.mariadb.jdbc.Driver");
@@ -23,10 +24,48 @@ public class UserControllerHelper {
         try(Connection con = DriverManager.getConnection(dbUrl, username, password)) {
             try (Statement stmt = con.createStatement()) {
                 stmt.executeUpdate(query);
-                Utils.LOGGER.info("Successfully deleted the user.");
             }
         } catch (SQLException e) {
-            System.out.println(e);
+            Utils.LOGGER.info(e);
         }
+    }
+
+    private static ResultSet getUser(String key, String value) {
+        String query = String.format(findQuery, key, value);
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(Connection con = DriverManager.getConnection(dbUrl, username, password)) {
+            try (Statement stmt = con.createStatement()) {
+                return stmt.executeQuery(query);
+            }
+        } catch (SQLException e) {
+            Utils.LOGGER.info(e);
+            return null;
+        }
+    }
+
+    private static boolean userExists(ResultSet resultSet) {
+        try {
+            if (resultSet!=null) return resultSet.next();
+            else {
+                Utils.LOGGER.info("Unsuccessful query.");
+            }
+        } catch (SQLException e) {
+            Utils.LOGGER.info(e);
+        }
+        return false;
+    }
+
+    public static void deleteUser(String key, String value) {
+        if (userExists(getUser(key, value))) delete(key, value);
+        else Utils.LOGGER.info("No user found.");
+
+        if (!userExists(getUser(key, value))) Utils.LOGGER.info("Successfully deleted user.");
+        else Utils.LOGGER.info("Deleting user was unsuccessful.");
     }
 }
