481c56f0eaacfe9a3e077b879c7c1c46b81414c3
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-13 19:22:36 +0300

    Added methods to PostHelper class
    - table specific query that selects every entry that matches given content and creation time
    - getPostsToDelete(String content, LocalDateTime dateTime)
    - getPostsToDeleteIds(String content, LocalDateTime dateTime)
    - deleteMultiplePostsById(ArrayList<Integer> ids)

--- TestAutomationFramework/src/main/java/com/testframework/databasehelper/PostHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/databasehelper/PostHelper.java
@@ -1,10 +1,17 @@
 package com.testframework.databasehelper;
 
+import com.testframework.FormatHelper;
+
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class PostHelper extends DatabaseHelper{
 
     private static final String tableName = "posts_table";
+    private static final String tableSpecificQuery =
+            "SELECT * FROM " + tableName + " WHERE content LIKE '%s' AND date_time LIKE '%s';";
 
     public static ResultSet getPost(String key, String value) {
         return getEntity(tableName, key, value);
@@ -20,6 +27,25 @@ public class PostHelper extends DatabaseHelper{
 
     public static int getPostByContent(String content) {
         return getEntityIdByKey("post", tableName, "content", content, "post_id");
+    }
+
+    public static ResultSet getPostsToDelete(String content, LocalDateTime dateTime) {
+        return executeQuery(String.format(tableSpecificQuery, content, FormatHelper.formatDateTime(dateTime)));
+    }
+
+    public static ArrayList<Integer> getPostsToDeleteIds(String content, LocalDateTime dateTime) {
+        ResultSet resultSet = getPostsToDelete(content, dateTime);
+        ArrayList<Integer> ids = new ArrayList<Integer>();
+        {
+            ids.add(getPostId(resultSet));
+        } while(entityExists(resultSet));
+        return ids;
+    }
+
+    public static void deleteMultiplePostsById(ArrayList<Integer> ids) {
+        for (int id : ids) {
+            deletePost("post_id", String.valueOf(id));
+        }
     }
 
 }
