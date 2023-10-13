ccab0c2d8bf221fa4fca58cb7aedbc17be4c7556
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-13 17:04:52 +0300

    Refactored api.models.Post - renamed it to RequestPost

--- TestAutomationFramework/src/main/java/com/testframework/api/models/RequestPost.java
+++ TestAutomationFramework/src/main/java/com/testframework/api/models/RequestPost.java
@@ -1,31 +1,41 @@
 package com.testframework.api.models;
 
 import com.testframework.generations.GenerateRandom;
+import com.testframework.models.Post;
+import com.testframework.models.enums.Visibility;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class Post {
+public class RequestPost {
     private int postId;
     private String content;
     private String picture;
     private boolean isPostPrivate;
 
-    public Post() {
+    public RequestPost() {
         setPostId(0);
         setContent(GenerateRandom.generateRandomBoundedAlphabeticString(20));
         setPicture("");
         setPostPrivate(false);
     }
 
+    public RequestPost(Post post) {
+        setPostId(0);
+        setContent(post.getContent());
+        setPicture("");
+        setPostPrivate(post.getVisibility().equals(Visibility.PRIVATE));
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Post p = (Post) o;
+        RequestPost p = (RequestPost) o;
 
         return  postId == p.postId
                 && content.equals(p.content)
                 && picture.equals(p.picture);
     }
+
 }
