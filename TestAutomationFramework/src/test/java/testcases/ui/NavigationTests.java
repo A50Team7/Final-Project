e31d8330ccc7c2a44401b175e1c90f81f2d1eaa7
Nikolay <n1k0lay93.ng@gmail.com>
2023-10-16 23:49:28 +0300

    NavigationTests UI Testing

---
+++ TestAutomationFramework/src/test/java/testcases/ui/NavigationTests.java
@@ -0,0 +1,139 @@
+package testcases.ui;
+
+import com.testframework.Utils;
+import com.testframework.api.RestUserController;
+import com.testframework.api.models.RequestUser;
+import com.testframework.factories.UserFactory;
+import com.testframework.models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class NavigationTests extends BaseTest {
+
+
+    private final static String ERROR_MESSAGE = "Navigated to a wrong url";
+    private final static String BASE_URL = Utils.getConfigPropertyByKey("weare.baseUrl");
+    private User user;
+    private String cookieValue;
+    public void setup() {
+        actions.getDriver().get("weare.baseUrl");
+    }
+
+    @Test
+    public void verify_RegisterButton_navigatesToPage() {
+        var regButton = By.xpath(Utils.getUIMappingByKey("navigation.registerButton"));
+        actions.waitForElementClickable(regButton);
+        actions.clickElement(regButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        String testUrl = Utils.getConfigPropertyByKey("weare.register.url");
+        Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
+    }
+
+
+    @Test
+    public void verify_HomeButton_navigatesToPage(){
+        actions.cleanDriver("weare.aboutus.url");
+
+        var homeButton = By.xpath(Utils.getUIMappingByKey("navigation.homeButton"));
+        actions.waitForElementClickable(homeButton);
+        actions.clickElement(homeButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        Assertions.assertEquals(BASE_URL, currentUrl, ERROR_MESSAGE);
+    }
+    @Test
+    public void verify_WEareButton_navigatesToPage(){
+        actions.cleanDriver("weare.aboutus.url");
+
+        var weareButton = By.xpath(Utils.getUIMappingByKey("navigation.WEareButton"));
+        actions.waitForElementClickable(weareButton);
+        actions.clickElement(weareButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        Assertions.assertEquals(BASE_URL, currentUrl, ERROR_MESSAGE);
+    }
+    @Test
+    public void verify_SignInButton_navigatesToPage(){
+        var logButton = By.xpath(Utils.getUIMappingByKey("navigation.signInButton"));
+        actions.waitForElementClickable(logButton);
+        actions.clickElement(logButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        String testUrl = Utils.getConfigPropertyByKey("weare.login.url");
+        Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
+    }
+
+    @Test
+    public void verify_LatestPosts_navigatesToPage() {
+        var postsButton = By.xpath(Utils.getUIMappingByKey("navigation.latestPostsButton"));
+        actions.waitForElementClickable(postsButton);
+        actions.clickElement(postsButton);
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        String testUrl = Utils.getConfigPropertyByKey("weare.latestposts.url");
+        Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
+    }
+    @Test
+    public void verify_AboutUs_navigatesToPage() {
+        var postsButton = By.xpath(Utils.getUIMappingByKey("navigation.aboutUsButton"));
+        actions.waitForElementClickable(postsButton);
+        actions.clickElement(postsButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        String testUrl = Utils.getConfigPropertyByKey("weare.aboutus.url");
+        Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
+    }
+
+    @Test
+    public void verify_PersonalProfile_navigatesToPage() {
+
+        user = UserFactory.createUser();
+        RestUserController.createUser(new RequestUser("ROLE_USER", user));
+        cookieValue = login(user);
+
+
+        actions.getDriver().get(BASE_URL);
+        //Add Profile Button Path
+        var profileButton = By.xpath(Utils.getUIMappingByKey("navigation.profileButton"));
+        actions.waitForElementClickable(profileButton);
+        actions.clickElement(profileButton);
+        //Assertions
+    }
+
+    @Test
+    public void verify_AddNewPost_navigatesToPage() {
+
+        user = UserFactory.createUser();
+        RestUserController.createUser(new RequestUser("ROLE_USER", user));
+        cookieValue = login(user);
+
+        actions.getDriver().get(BASE_URL);
+
+        var postsButton = By.xpath(Utils.getUIMappingByKey("navigation.newPostButton"));
+        actions.waitForElementClickable(postsButton);
+        actions.clickElement(postsButton);
+
+        String currentUrl = actions.getDriver().getCurrentUrl();
+        String testUrl = Utils.getConfigPropertyByKey("weare.createpost.url");
+        Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
+    }
+
+    @Test
+    public void verify_LOGOUT() {
+        //Register user -> Login ->
+        user = UserFactory.createUser();
+        RestUserController.createUser(new RequestUser("ROLE_USER", user));
+        cookieValue = login(user);
+
+        actions.getDriver().get(BASE_URL);
+
+        var logoutButton = By.xpath(Utils.getUIMappingByKey("navigation.logoutButton"));
+        actions.waitForElementClickable(logoutButton);
+        actions.clickElement(logoutButton);
+
+        var testElement = By.xpath(Utils.getUIMappingByKey("navigation.signInButton"));
+        actions.assertElementPresent(testElement);
+    }
+}
