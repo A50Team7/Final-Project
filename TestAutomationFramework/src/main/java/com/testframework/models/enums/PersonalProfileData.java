6f4d943ebe697ca2c2bed627e68c04f4d9dd33f4
KaterinaKN89 <nedyalkovakaterina@gmail.com>
2023-10-17 11:57:45 +0300

    Update PersonalProfileData.java
    Edited the PersonalProfile enum fields

--- TestAutomationFramework/src/main/java/com/testframework/models/enums/PersonalProfileData.java
+++ TestAutomationFramework/src/main/java/com/testframework/models/enums/PersonalProfileData.java
@@ -3,10 +3,18 @@ package com.testframework.models.enums;
 public enum PersonalProfileData {
 
     USER_ID,
-    NAME,
+    FIRST_NAME,
+    LAST_NAME,
+    FULL_NAME,
     EMAIL,
     BIRTHDAY,
     LOCATION,
     FRIEND_LIST,
-    BIO;
+    BIO,
+    PROFESSIONAL_CATEGORY,
+    SERVICE_ONE,
+    SERVICE_TWO,
+    SERVICE_THREE,
+    SERVICE_FOUR,
+    SERVICE_FIVE;
 }
