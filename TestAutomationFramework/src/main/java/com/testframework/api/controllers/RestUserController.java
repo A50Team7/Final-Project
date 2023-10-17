7a0068df0f81fa870d167abee3da22b815c42bd9
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-17 22:03:13 +0300

    Refactored upgradeUserExpertiseProfile() and upgradeUserPersonalProfile(), and added getAllProfilePosts() in RestUserController

--- TestAutomationFramework/src/main/java/com/testframework/api/controllers/RestUserController.java
+++ TestAutomationFramework/src/main/java/com/testframework/api/controllers/RestUserController.java
@@ -9,7 +9,6 @@ import static io.restassured.RestAssured.given;
 
 public class RestUserController {
 
-
     public static Response createUser(UserRequest user) {
         return given()
                 .contentType(ContentType.JSON)
@@ -38,17 +37,18 @@ public class RestUserController {
         return given()
                 .queryParam("principal", principal)
                 .when()
-                .get("/users/auth/" + String.valueOf(id))
+                .get("/users/auth/" + id)
                 .then()
                 .assertThat().statusCode(200)
                 .extract().response().as(UserResponse.class);
     }
 
-    public static ExpertiseProfileResponse upgradeUserExpertiseProfile(int userId, ExpertiseProfileRequest request) {
+    public static ExpertiseProfileResponse upgradeUserExpertiseProfile(int userId, ExpertiseProfileRequest request, String cookieValue) {
         return given()
                 .contentType(ContentType.JSON)
                 .and()
                 .body(request)
+                .cookie("JSESSIONID", cookieValue)
                 .when()
                 .post(String.format("/users/auth/%s/expertise", userId))
                 .then()
@@ -56,16 +56,30 @@ public class RestUserController {
                 .extract().response().as(ExpertiseProfileResponse.class);
     }
 
-    public static PersonalProfileResponse upgradeUserPersonalProfile(int userId, PersonalProfileRequest request) {
+    public static PersonalProfileResponse upgradeUserPersonalProfile(int userId, PersonalProfileRequest request, String cookieValue) {
         return given()
                 .contentType(ContentType.JSON)
                 .and()
                 .body(request)
+                .cookie("JSESSIONID", cookieValue)
                 .when()
                 .post("/users/auth/" + userId + "/personal")
                 .then()
                 .assertThat().statusCode(200)
                 .extract().response().as(PersonalProfileResponse.class);
+    }
+
+    public static PostResponse[] getAllProfilePosts(ProfilePostsRequest request, int userId, String cookieValue) {
+        return given()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(request)
+                .cookie("JSESSIONID", cookieValue)
+                .when()
+                .get(String.format("/users/%s/posts", userId))
+                .then()
+                .assertThat().statusCode(200)
+                .extract().response().as(PostResponse[].class);
     }
 
     public static Response authUser(String username, String password) {
@@ -75,4 +89,5 @@ public class RestUserController {
                 .post(Utils.getConfigPropertyByKey("weare.auth.url"));
 
     }
+
 }
