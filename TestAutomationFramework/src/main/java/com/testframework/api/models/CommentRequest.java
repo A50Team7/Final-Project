838230d889963c3d0fd5ff0b24958623019f5d16
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-18 13:44:02 +0300

    Refactored CommentReqeust

--- TestAutomationFramework/src/main/java/com/testframework/api/models/CommentRequest.java
+++ TestAutomationFramework/src/main/java/com/testframework/api/models/CommentRequest.java
@@ -1,39 +1,21 @@
 package com.testframework.api.models;
 
 import com.testframework.generations.GenerateRandom;
+import com.testframework.models.Comment;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class CommentRequest {
+
+    public CommentRequest(Comment comment) {
+        setUserId(comment.getAuthor().getUserId());
+        setPostId(comment.getPost().getPostId());
+        setContent(comment.getContent());
+    }
+
     private int userId;
     private int postId;
     private String content;
 
-    public CommentRequest(int userId, int postId) {
-        setUserId(userId);
-        setPostId(postId);
-        setContent(GenerateRandom.generateRandomBoundedAlphabeticString(15));
-    }
-
-    private int getUserId() {
-        return userId;
-    }
-
-    private void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    private int getPostId() {
-        return postId;
-    }
-
-    private void setPostId(int postId) {
-        this.postId = postId;
-    }
-
-    private String getContent() {
-        return content;
-    }
-
-    private void setContent(String content) {
-        this.content = content;
-    }
 }
