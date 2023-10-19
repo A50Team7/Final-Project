f97c76d9b4b2061d39f348e303ff5966db56df33
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-19 23:18:46 +0300

    Added LoginTests

---
+++ TestAutomationFramework/src/test/java/testcases/ui/LoginTests.java
@@ -0,0 +1,94 @@
+package testcases.ui;
+
+import com.testframework.Utils;
+import com.testframework.api.controllers.RestUserController;
+import com.testframework.api.models.UserRequest;
+import com.testframework.factories.UserFactory;
+import com.testframework.models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+import pages.homepage.UserHomePage;
+
+public class LoginTests extends BaseTest {
+    private static String loginPageUrl = Utils.getConfigPropertyByKey("weare.login.url");
+    private static String baseUrl = Utils.getConfigPropertyByKey("weare.baseUrl");
+    private static String cookieName = Utils.getConfigPropertyByKey("auth.cookieName");
+    private static LoginPage loginPage = new LoginPage(actions.getDriver(), loginPageUrl);
+    private static UserHomePage homePage = new UserHomePage(actions.getDriver(), baseUrl);
+    private User unregisteredUser;
+
+    @BeforeEach
+    public void registerUser() {
+        user = UserFactory.createUser();
+        unregisteredUser = UserFactory.createUser();
+        RestUserController.createUser(new UserRequest("ROLE_USER", user));
+
+        loginPage.navigateToPage();
+    }
+
+    @Test
+    public void loginWithValidCredentials_Should_BeSuccessful() {
+        loginPage.enterAllCredentialsAndLogin(user.getUsername(), user.getPassword());
+
+        homePage.assertLogoutButtonPresent();
+        assertAuthCookiePresent();
+    }
+
+    @Test
+    public void loginWithBothCredentialsInvalid_Should_BeUnsuccessful() {
+        loginPage.enterAllCredentialsAndLogin(unregisteredUser.getUsername(), unregisteredUser.getPassword());
+
+        assertUnsuccessfulLogin();
+    }
+
+    @Test
+    public void loginWithDifferentUsername_Should_BeUnsuccessful() {
+        loginPage.enterAllCredentialsAndLogin(unregisteredUser.getUsername(), user.getPassword());
+
+        assertUnsuccessfulLogin();
+    }
+
+    @Test
+    public void loginWithDifferentPassword_Should_BeUnsuccessful() {
+        loginPage.enterAllCredentialsAndLogin(user.getUsername(), unregisteredUser.getPassword());
+
+        assertUnsuccessfulLogin();
+    }
+
+    @Test
+    public void loginWithoutEnteringPassword_Should_BeUnsuccessful() {
+        loginPage.enterUsername(user.getUsername());
+        loginPage.login();
+
+        assertUnsuccessfulLogin();
+    }
+
+    @Test
+    public void loginWithoutEnteringUsername_Should_BeUnsuccessful() {
+        loginPage.enterPassword(user.getUsername());
+        loginPage.login();
+
+        assertUnsuccessfulLogin();
+    }
+
+    @Test
+    public void loginWithoutEnteringBothCredentials_Should_BeUnsuccessful() {
+        loginPage.login();
+
+        assertUnsuccessfulLogin();
+    }
+
+    private void assertAuthCookiePresent() {
+        Assertions.assertTrue(actions.cookieExists(cookieName), "Auth cookie is not present.");
+    }
+
+    private void assertNoAuthCookiePresent() {
+        Assertions.assertFalse(actions.cookieExists(cookieName), "Auth cookie is present.");
+    }
+
+    private void assertUnsuccessfulLogin() {
+        loginPage.assertErrorMessagePresent("Wrong username or password");
+    }
+}
