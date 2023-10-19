7b1a476ba5106171e0f630fec548c20d9e543af8
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-19 15:45:16 +0300

    Fixed the field corresponding with 'public' in EditPostRequest

--- TestAutomationFramework/src/main/java/com/testframework/api/models/EditPostRequest.java
+++ TestAutomationFramework/src/main/java/com/testframework/api/models/EditPostRequest.java
@@ -1,5 +1,6 @@
 package com.testframework.api.models;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,10 @@ public class EditPostRequest {
     public EditPostRequest(String newContent) {
         setContent(newContent);
         setPicture("");
-        setPrivate(true);
+        setPublic(true);
     }
 
     private String content;
     private String picture;
-    private boolean isPrivate;
-
+    @SerializedName("public") private boolean isPublic;
 }
