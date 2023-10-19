6991a1ef126ceaac2666037b19c6d203185d8de4
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-19 20:26:05 +0300

    Added POM for the login page

---
+++ TestAutomationFramework/src/main/java/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import com.testframework.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pages.common.BasePage;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+    private static By usernameBy = By.xpath(Utils.getUIMappingByKey("login.username"));
+    private static By passwordBy = By.xpath(Utils.getUIMappingByKey("login.password"));
+    private static By submitBy = By.xpath(Utils.getUIMappingByKey("login.submit"));
+    private static String errorMessage = Utils.getUIMappingByKey("registerAndLogin.errorMessage");
+
+    public void assertErrorMessagePresent(String text) {
+        By errorBy = By.xpath(String.format(errorMessage, text));
+        actions.waitForElementPresent(errorBy);
+        actions.assertElementPresent(errorBy);
+    }
+
+    public void enterAllCredentialsAndLogin(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        login();
+    }
+
+    public void enterUsername(String username) {
+        actions.clearAndTypeValueInField(usernameBy, username);
+    }
+
+    public void enterPassword(String password) {
+        actions.clearAndTypeValueInField(passwordBy, password);
+    }
+
+    public void login() {
+        actions.clickElement(submitBy);
+    }
+}
