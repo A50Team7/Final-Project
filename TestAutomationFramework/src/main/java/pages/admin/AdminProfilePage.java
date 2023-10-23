ef6ee4928a17a71560e8a136e2378f574dcaac99
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-23 13:33:52 +0300

    Create AdminProfilePage.java

---
+++ TestAutomationFramework/src/main/java/pages/admin/AdminProfilePage.java
@@ -0,0 +1,40 @@
+package pages.admin;
+
+import com.testframework.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pages.PersonalProfilePage;
+
+public class AdminProfilePage extends PersonalProfilePage {
+    public AdminProfilePage(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+    private static By disableBy = By.xpath(Utils.getUIMappingByKey("admin.disableUserButton"));
+    private static By enableBy = By.xpath(Utils.getUIMappingByKey("admin.enableUserButton"));
+
+    public void disable() {
+        assertDisableButtonPresent();
+        actions.clickElement(disableBy);
+    }
+
+    public void enable() {
+        assertEnableButtonPresent();
+        actions.clickElement(enableBy);
+    }
+
+    public void assertEnableButtonPresent() {
+        actions.waitForElementPresent(enableBy);
+        actions.assertElementPresent(enableBy);
+    }
+
+    public void assertDisableButtonPresent() {
+        actions.waitForElementPresent(disableBy);
+        actions.assertElementPresent(disableBy);
+    }
+
+    public void assertEditProfilePresent() {
+        actions.waitForElementPresent(editProfileBy);
+        actions.assertElementPresent(editProfileBy);
+    }
+}
