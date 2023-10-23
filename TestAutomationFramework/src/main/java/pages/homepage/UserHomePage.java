9434c19e1e79f543669f72339821c429fe12fa6a
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-23 13:34:56 +0300

    Create UserHomePage.java

---
+++ TestAutomationFramework/src/main/java/pages/homepage/UserHomePage.java
@@ -0,0 +1,33 @@
+package pages.homepage;
+
+import com.testframework.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UserHomePage extends HomePage{
+
+    public UserHomePage(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+    private static By logoutBy = By.xpath(Utils.getUIMappingByKey("user.homepage.logoutButton"));
+    private static By newPostBy = By.xpath(Utils.getUIMappingByKey("user.homepage.newPostButton"));
+    private static By profileBy = By.xpath(Utils.getUIMappingByKey("user.homepage.profileButton"));
+
+    public void logout() {
+        actions.clickElement(logoutBy);
+    }
+
+    public void goToNewPost() {
+        actions.clickElement(newPostBy);
+    }
+
+    public void goToPersonalProfile() {
+        actions.clickElement(profileBy);
+    }
+
+    public void assertLogoutButtonPresent() {
+        actions.waitForElementPresent(logoutBy);
+        actions.assertElementPresent(logoutBy);
+    }
+}
