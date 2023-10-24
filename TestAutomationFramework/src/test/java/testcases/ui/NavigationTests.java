82546d94c5a79dcb7e21f1a06c4ebc364146aacd
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-24 10:51:48 +0300

    Refactored NavigationTests

--- TestAutomationFramework/src/test/java/testcases/ui/NavigationTests.java
+++ TestAutomationFramework/src/test/java/testcases/ui/NavigationTests.java
@@ -1,29 +1,33 @@
 package testcases.ui;
 
 import com.testframework.Utils;
+import com.testframework.WaitHelper;
 import com.testframework.api.controllers.RestUserController;
 import com.testframework.api.models.UserRequest;
 import com.testframework.factories.UserFactory;
 import com.testframework.models.User;
 import io.restassured.response.ResponseBody;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import pages.homepage.AnonymousHomePage;
+import pages.homepage.UserHomePage;
 
 public class NavigationTests extends BaseTest {
 
 
     private final static String ERROR_MESSAGE = "Navigated to a wrong url";
     private static String baseUrl = Utils.getConfigPropertyByKey("weare.baseUrl");
+    private static AnonymousHomePage anonHomePage = new AnonymousHomePage(actions.getDriver(), baseUrl);
+    private static UserHomePage userHomePage = new UserHomePage(actions.getDriver(), baseUrl);
     private User user;
     private String cookieValue;
 
-
     @Test
     public void verify_RegisterButton_navigatesToPage() {
-        var regButton = By.xpath(Utils.getUIMappingByKey("anon.homepage.registerButton"));
-        actions.waitForElementClickable(regButton);
-        actions.clickElement(regButton);
+        anonHomePage.navigateToPage();
+        anonHomePage.goToRegisterPage();
 
         String currentUrl = actions.getDriver().getCurrentUrl();
         String testUrl = Utils.getConfigPropertyByKey("weare.register.url");
@@ -36,7 +40,6 @@ public class NavigationTests extends BaseTest {
         actions.cleanDriver("weare.aboutus.url");
 
         var homeButton = By.xpath(Utils.getUIMappingByKey("navigation.homeButton"));
-        actions.waitForElementClickable(homeButton);
         actions.clickElement(homeButton);
 
         String currentUrl = actions.getDriver().getCurrentUrl();
@@ -46,10 +49,7 @@ public class NavigationTests extends BaseTest {
     @Test
     public void verify_WEareButton_navigatesToPage() {
         actions.cleanDriver("weare.aboutus.url");
-
-        var weareButton = By.xpath(Utils.getUIMappingByKey("navigation.WEareButton"));
-        actions.waitForElementClickable(weareButton);
-        actions.clickElement(weareButton);
+        anonHomePage.clickWEareButton();
 
         String currentUrl = actions.getDriver().getCurrentUrl();
         Assertions.assertEquals(baseUrl, currentUrl, ERROR_MESSAGE);
@@ -57,9 +57,7 @@ public class NavigationTests extends BaseTest {
 
     @Test
     public void verify_SignInButton_navigatesToPage() {
-        var logButton = By.xpath(Utils.getUIMappingByKey("anon.homepage.loginButton"));
-        actions.waitForElementClickable(logButton);
-        actions.clickElement(logButton);
+        anonHomePage.goToLoginPage();
 
         String currentUrl = actions.getDriver().getCurrentUrl();
         String testUrl = Utils.getConfigPropertyByKey("weare.login.url");
@@ -68,9 +66,8 @@ public class NavigationTests extends BaseTest {
 
     @Test
     public void verify_LatestPosts_navigatesToPage() {
-        var postsButton = By.xpath(Utils.getUIMappingByKey("homepage.latestPosts"));
-        actions.waitForElementClickable(postsButton);
-        actions.clickElement(postsButton);
+        anonHomePage.goToLatestPosts();
+
         String currentUrl = actions.getDriver().getCurrentUrl();
         String testUrl = Utils.getConfigPropertyByKey("weare.latestposts.url");
         Assertions.assertEquals(testUrl, currentUrl, ERROR_MESSAGE);
@@ -78,9 +75,7 @@ public class NavigationTests extends BaseTest {
 
     @Test
     public void verify_AboutUs_navigatesToPage() {
-        var aboutusButton = By.xpath(Utils.getUIMappingByKey("navigation.aboutUsButton"));
-        actions.waitForElementClickable(aboutusButton);
-        actions.clickElement(aboutusButton);
+        anonHomePage.clickAboutUsButton();
 
         String currentUrl = actions.getDriver().getCurrentUrl();
         String testUrl = Utils.getConfigPropertyByKey("weare.aboutus.url");
@@ -90,17 +85,14 @@ public class NavigationTests extends BaseTest {
     @Test
     public void verify_PersonalProfile_navigatesToPage() {
         user = UserFactory.createUser();
-        var createdUser = RestUserController.createUser(new UserRequest("ROLE_USER", user));
+        RestUserController.createUser(new UserRequest("ROLE_USER", user));
         cookieValue = login(user);
 
         int userId = user.getUserId();
 
-        actions.getDriver().get(baseUrl);
+        userHomePage.navigateToPage();
+        userHomePage.goToPersonalProfile();
 
-        var profileButton = By.xpath(Utils.getUIMappingByKey("user.homepage.profileButton"));
-        actions.waitForElementClickable(profileButton);
-        actions.clickElement(profileButton);
-        //Assertions
         String profileUrl = actions.getDriver().getCurrentUrl();
         String testUrl = String.format(Utils.getConfigPropertyByKey("weare.profile.url"), userId);
         Assertions.assertEquals(testUrl, profileUrl, ERROR_MESSAGE);
@@ -108,16 +100,12 @@ public class NavigationTests extends BaseTest {
 
     @Test
     public void verify_AddNewPost_navigatesToPage() {
-
         user = UserFactory.createUser();
         RestUserController.createUser(new UserRequest("ROLE_USER", user));
         cookieValue = login(user);
 
-        actions.getDriver().get(baseUrl);
-
-        var postsButton = By.xpath(Utils.getUIMappingByKey("user.homepage.newPostButton"));
-        actions.waitForElementClickable(postsButton);
-        actions.clickElement(postsButton);
+        userHomePage.navigateToPage();
+        userHomePage.goToNewPost();
 
         String currentUrl = actions.getDriver().getCurrentUrl();
         String testUrl = Utils.getConfigPropertyByKey("weare.createpost.url");
@@ -126,16 +114,12 @@ public class NavigationTests extends BaseTest {
 
     @Test
     public void verify_LOGOUT() {
-        //Register user -> Login ->
         user = UserFactory.createUser();
         RestUserController.createUser(new UserRequest("ROLE_USER", user));
         cookieValue = login(user);
 
-        actions.getDriver().get(baseUrl);
-
-        var logoutButton = By.xpath(Utils.getUIMappingByKey("user.homepage.logoutButton"));
-        actions.waitForElementClickable(logoutButton);
-        actions.clickElement(logoutButton);
+        userHomePage.navigateToPage();
+        userHomePage.logout();
 
         var testElement = By.xpath(Utils.getUIMappingByKey("anon.homepage.loginButton"));
         actions.assertElementPresent(testElement);
