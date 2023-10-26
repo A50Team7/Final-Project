8694089151b0183c51039fc778b0feb5c2671f88
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:41:12 +0300

    Added javadoc to Utils

--- TestAutomationFramework/src/main/java/com/testframework/Utils.java
+++ TestAutomationFramework/src/main/java/com/testframework/Utils.java
@@ -5,26 +5,49 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import java.util.Properties;
 
+/**
+ * Utility class providing various helper methods for the testing framework.
+ */
 public class Utils {
 
     private static final Properties uiMappings = PropertiesManager.PropertiesManagerEnum.INSTANCE.getUiMappings();
     private static final Properties configProperties = PropertiesManager.PropertiesManagerEnum.INSTANCE.getConfigProperties();
     public static final Logger LOGGER = LogManager.getRootLogger();
 
+    /**
+     * Retrieves the current WebDriver instance.
+     *
+     * @return the current WebDriver instance
+     */
     public static WebDriver getWebDriver() {
         return CustomWebDriverManager.CustomWebDriverManagerEnum.INSTANCE.getDriver();
     }
 
+    /**
+     * Tears down the current WebDriver instance.
+     */
     public static void tearDownWebDriver() {
         LOGGER.info("Quitting WebDriver");
         CustomWebDriverManager.CustomWebDriverManagerEnum.INSTANCE.quitDriver();
     }
 
+    /**
+     * Retrieves the locator from the ui_map file by the specified key.
+     *
+     * @param key the name of the field
+     * @return the string value of the field, or the key if not found
+     */
     public static String getUIMappingByKey(String key) {
         String value = uiMappings.getProperty(key);
         return value != null ? value : key;
     }
 
+    /**
+     * Retrieves the URL or constants from the config file by the specified key.
+     *
+     * @param key the name of the field
+     * @return the string value of the field, or the key if not found
+     */
     public static String getConfigPropertyByKey(String key) {
         String value = configProperties.getProperty(key);
         return value != null ? value : key;
