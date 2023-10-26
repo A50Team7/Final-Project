640e0612a6f6f17a265d56942e3a2e3d85dd891d
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:08:59 +0300

    Added javadoc to RequestsHelper

--- TestAutomationFramework/src/main/java/com/testframework/databasehelper/RequestsHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/databasehelper/RequestsHelper.java
@@ -1,9 +1,17 @@
 package com.testframework.databasehelper;
 
+/**
+ * A utility class for managing requests-related database operations, extending DatabaseHelper.
+ *
+ * @see DatabaseHelper
+ */
 public class RequestsHelper extends DatabaseHelper{
 
     private static String truncateRequestsTableQuery = "TRUNCATE TABLE requests;";
 
+    /**
+     * Truncates the requests table in the database.
+     */
     public static void truncateRequestsTable() {
         DatabaseHelper.executeQuery(truncateRequestsTableQuery);
     }
