b2f6c6d8938bd37483f1de78f084cef2fe93bb42
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 16:43:20 +0300

    Moved getCookieValue() method from ApiHelper to BaseApiTest

--- TestAutomationFramework/src/test/java/testcases/api/BaseApiTest.java
+++ TestAutomationFramework/src/test/java/testcases/api/BaseApiTest.java
@@ -1,8 +1,11 @@
 package testcases.api;
 
 import com.testframework.Utils;
+import com.testframework.api.controllers.RestUserController;
+import com.testframework.models.User;
 import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseApiTest {
@@ -14,5 +17,17 @@ public class BaseApiTest {
         RestAssured.config = RestAssured.config().encoderConfig(encoderConfig);
 
         RestAssured.baseURI = Utils.getConfigPropertyByKey("weare.api.url");
+    }
+
+    /**
+     * Retrieves the value of the cookie for the specified user using API.
+     *
+     * @param user the user for whom the cookie value is to be retrieved
+     * @return the value of the cookie associated with the user
+     */
+    public static String getCookieValue(User user) {
+        String name = Utils.getConfigPropertyByKey("auth.cookieName");
+        Response auth = RestUserController.authUser(user.getUsername(), user.getPassword());
+        return auth.getDetailedCookie(name).getValue();
     }
 }
