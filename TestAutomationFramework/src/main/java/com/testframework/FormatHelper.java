17edefd87f3f172b8babf151778c363d7f1daa01
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:14:10 +0300

    Added javadoc to FormatHelper and refactored some of the methods

--- TestAutomationFramework/src/main/java/com/testframework/FormatHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/FormatHelper.java
@@ -1,10 +1,7 @@
 package com.testframework;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.regex.Matcher;
@@ -12,28 +9,67 @@ import java.util.regex.Pattern;
 
 public class FormatHelper {
 
+    /**
+     * Formats a LocalDateTime object into a string representation using the "dd/MM/yyyy HH:mm:ss" format.
+     *
+     * @param dateTime the LocalDateTime object to be formatted
+     * @return the formatted string
+     */
     public static String formatDateTime(LocalDateTime dateTime) {
-        ZonedDateTime zonedDateTime = dateTime.atZone(ZoneId.systemDefault());
-        ZonedDateTime utcDateTime = zonedDateTime.withZoneSameInstant(ZoneOffset.UTC);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        return utcDateTime.format(formatter);
+        return dateTime.format(formatter);
     }
 
-    public static String formatBirthdayDate(Date date) {
+    /**
+     * Parses a string representation of a date and time into a LocalDateTime object using the "dd/MM/yyyy HH:mm:ss" format.
+     *
+     * @param dateTime the string representation of the date and time
+     * @return the parsed LocalDateTime object
+     */
+    public static LocalDateTime parseDateTimeFromString(String dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return LocalDateTime.parse(dateTime, formatter);
+    }
+
+    /**
+     * Formats a LocalDate object into a string representation using the "yyyy-MM-dd" format.
+     *
+     * @param date the LocalDate object to be formatted
+     * @return the formatted string
+     */
+    public static String formatBirthdayDate(LocalDate date) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         return formatter.format(date);
     }
 
-    public static String formatRegistrationDate(Date date) {
+    /**
+     * Formats a LocalDate object into a string representation using the "dd/MM/yyyy" format.
+     *
+     * @param date the LocalDate object to be formatted
+     * @return the formatted string
+     */
+    public static String formatRegistrationDate(LocalDate date) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         return formatter.format(date);
     }
 
-    public static String formatDateAmericanFormat(Date date) {
+    /**
+     * Formats a LocalDate object into a string representation using the "MM/dd/yyyy" format.
+     *
+     * @param date the LocalDate object to be formatted
+     * @return the formatted string
+     */
+    public static String formatDateAmericanFormat(LocalDate date) {
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
         return formatter.format(date);
     }
 
+    /**
+     * Extracts the first number found in the provided text.
+     *
+     * @param text the text from which to extract the number
+     * @return the extracted number, or -1 if no number is found
+     */
     public static int extractNumber(String text) {
         try {
             Pattern pattern = Pattern.compile("\\b(\\d+)\\b");
@@ -43,7 +79,7 @@ public class FormatHelper {
                 return Integer.parseInt(numberString);
             }
         } catch (NumberFormatException | IllegalStateException | IndexOutOfBoundsException | NullPointerException e) {
-            e.printStackTrace();
+            Utils.LOGGER.info(e);
         }
         return -1;
     }
