7475fe6ccf277c3e5a0b7d8898d7b758b891a29a
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:41:47 +0300

    Added javadoc to CustomWebDriverManager class and refactored the setupBrowser() method

--- TestAutomationFramework/src/main/java/com/testframework/CustomWebDriverManager.java
+++ TestAutomationFramework/src/main/java/com/testframework/CustomWebDriverManager.java
@@ -13,12 +13,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * CustomWebDriverManager class providing a way to manage the WebDriver for custom web browsing operations.
+ * Uses CustomWebDriverManagerEnum internally for managing the WebDriver instance.
+ */
 public class CustomWebDriverManager {
 
+    /**
+     * CustomWebDriverManagerEnum for managing the WebDriver instance.
+     */
     public enum CustomWebDriverManagerEnum {
         INSTANCE;
         private WebDriver driver = setupBrowser();
 
+        /**
+         * Quits the WebDriver instance if it exists.
+         */
         public void quitDriver() {
             if (driver != null) {
                 driver.quit();
@@ -26,6 +36,11 @@ public class CustomWebDriverManager {
             }
         }
 
+        /**
+         * Retrieves the WebDriver instance. If the instance doesn't exist, it initializes the WebDriver.
+         *
+         * @return the WebDriver instance
+         */
         public WebDriver getDriver() {
             if (driver == null) {
                 Utils.LOGGER.info("Initializing WebDriver");
@@ -35,7 +50,10 @@ public class CustomWebDriverManager {
         }
 
         private WebDriver setupBrowser() {
-            WebDriver driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-timezone-tracking");
+            options.addArguments("--user-timezone=utc");
+            WebDriver driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             this.driver = driver;
             return driver;
