558be1245df17a91611d1114ed6d2eb93e887ffd
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:09:16 +0300

    Added javadoc to ConnectionHelper

--- TestAutomationFramework/src/main/java/com/testframework/databasehelper/ConnectionHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/databasehelper/ConnectionHelper.java
@@ -1,9 +1,17 @@
 package com.testframework.databasehelper;
 
+/**
+ * A utility class for managing connection-related database operations, extending DatabaseHelper.
+ *
+ * @see DatabaseHelper
+ */
 public class ConnectionHelper extends DatabaseHelper{
 
     private static String truncateConnectionsTableQuery = "TRUNCATE TABLE connections_table;";
 
+    /**
+     * Truncates the requests table in the database.
+     */
     public static void truncateConnectionsTable() {
         DatabaseHelper.executeQuery(truncateConnectionsTableQuery);
     }
