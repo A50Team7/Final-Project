89068f98bddc75e1a9d487ee71b1cd5d1ee2dd7d
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 16:36:59 +0300

    Refactored FormatHelper

--- TestAutomationFramework/src/main/java/com/testframework/FormatHelper.java
+++ TestAutomationFramework/src/main/java/com/testframework/FormatHelper.java
@@ -16,6 +16,7 @@ public class FormatHelper {
      * @return the formatted string
      */
     public static String formatDateTime(LocalDateTime dateTime) {
+        if (dateTime == null) return null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         return dateTime.format(formatter);
     }
@@ -27,6 +28,7 @@ public class FormatHelper {
      * @return the parsed LocalDateTime object
      */
     public static LocalDateTime parseDateTimeFromString(String dateTime) {
+        if (dateTime == null) return null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         return LocalDateTime.parse(dateTime, formatter);
     }
@@ -38,8 +40,21 @@ public class FormatHelper {
      * @return the formatted string
      */
     public static String formatBirthdayDate(LocalDate date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        return formatter.format(date);
+        if (date == null) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return date.format(formatter);
+    }
+
+    /**
+     * Parses a string representation of a date into a LocalDate object using the "yyyy-MM-dd" format.
+     *
+     * @param dateTime the string representation of the date and time
+     * @return the parsed LocalDate object
+     */
+    public static LocalDate parseDateFromString(String dateTime) {
+        if (dateTime == null) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDate.parse(dateTime, formatter);
     }
 
     /**
@@ -49,8 +64,9 @@ public class FormatHelper {
      * @return the formatted string
      */
     public static String formatRegistrationDate(LocalDate date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        return formatter.format(date);
+        if (date == null) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return date.format(formatter);
     }
 
     /**
@@ -60,8 +76,9 @@ public class FormatHelper {
      * @return the formatted string
      */
     public static String formatDateAmericanFormat(LocalDate date) {
-        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-        return formatter.format(date);
+        if (date == null) return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        return date.format(formatter);
     }
 
     /**
