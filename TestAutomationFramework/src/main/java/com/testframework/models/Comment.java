93a248a48d3008a72968487f438fc73536d755f7
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:22:32 +0300

    Added javadoc to models.Comment

--- TestAutomationFramework/src/main/java/com/testframework/models/Comment.java
+++ TestAutomationFramework/src/main/java/com/testframework/models/Comment.java
@@ -1,42 +1,47 @@
 package com.testframework.models;
 
+import com.testframework.Utils;
 import com.testframework.databasehelper.CommentHelper;
 import com.testframework.models.interfaces.Likable;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
+/**
+ * Comment model representing the comments in the application.
+ * Instances can be created using the CommentFactory class.
+ *
+ * @see com.testframework.factories.CommentFactory
+ */
 @Getter @Setter
 public class Comment implements Likable {
 
+    /**
+     * Constructs a Comment with the provided post, author, and content.
+     * The creation date and time are set to the current date based on the configured time zone.
+     * The comment is added to the corresponding post.
+     *
+     * @param post the post the comment is associated with
+     * @param author the author of the comment
+     * @param content the content of the comment
+     */
     public Comment(Post post, User author, String content) {
         setPost(post);
         setAuthor(author);
-        setCreationDateTime(LocalDateTime.now());
+        setCreationDateTime(LocalDateTime.now(ZoneId.of(Utils.getConfigPropertyByKey("weare.timeZone"))));
         setContent(content);
         likes = 0;
         post.addComment(this);
     }
 
-    private int commentId;
     private Post post;
     private User author;
     private LocalDateTime creationDateTime;
     private String content;
     private int likes;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Comment c = (Comment) o;
-
-        return author.equals(c.author)
-                && creationDateTime.equals(c.creationDateTime)
-                && content.equals(c.content)
-                && likes==c.likes;
-    }
 
     public void like() {
         likes++;
@@ -46,6 +51,13 @@ public class Comment implements Likable {
         likes--;
     }
 
+    /**
+     * Retrieves the comment ID by searching the database for the comment's content.
+     * Connects to the database using CommentHelper.
+     *
+     * @return the comment ID if found, or -1 if not found
+     * @see CommentHelper
+     */
     public int getCommentId() {
         return CommentHelper.getCommentId(CommentHelper.getComment("content", String.format("'%s'", this.content)));
     }
