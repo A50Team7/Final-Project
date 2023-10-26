d8b252f5ed9c76fe8ab64d1c7d5e934a94ce0fdb
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2023-10-26 11:17:15 +0300

    Added javadoc to models.User and refactored the constructors in how they create the registration date

--- TestAutomationFramework/src/main/java/com/testframework/models/User.java
+++ TestAutomationFramework/src/main/java/com/testframework/models/User.java
@@ -1,30 +1,60 @@
 package com.testframework.models;
 
+import com.testframework.Utils;
 import com.testframework.databasehelper.UserHelper;
 import com.testframework.models.enums.ProfessionalCategory;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
+/**
+ * User model representing users in the application.
+ * Instances can be created using the UserFactory class.
+ *
+ * @see com.testframework.factories.UserFactory
+ */
 @Getter @Setter
 public class User {
+
+    /**
+     * Constructs a User object with the provided username, email, password, and category.
+     * The registration date is set to the current date based on the configured time zone.
+     *
+     * @param username the username of the user
+     * @param email the email of the user
+     * @param password the password of the user
+     * @param category the professional category of the user
+     */
     public User(String username, String email, String password, ProfessionalCategory category) {
         setUsername(username);
         setEmail(email);
         setPassword(password);
         setCategory(category);
-        setRegistrationDate(Date.from(Instant.now()));
+        setRegistrationDate(LocalDate.now(ZoneId.of(Utils.getConfigPropertyByKey("weare.timeZone"))));
     }
 
+    /**
+     * Constructs a User object with the provided username, email, password, category, and profile.
+     * The registration date is set to the current date based on the configured time zone.
+     *
+     * @param username the username of the user
+     * @param email the email of the user
+     * @param password the password of the user
+     * @param category the professional category of the user
+     * @param profile the profile of the user
+     */
     public User(String username, String email, String password, ProfessionalCategory category, Profile profile) {
         setUsername(username);
         setEmail(email);
         setPassword(password);
         setCategory(category);
         setProfile(profile);
-        setRegistrationDate(Date.from(Instant.now()));
+        setRegistrationDate(LocalDate.now(ZoneId.of(Utils.getConfigPropertyByKey("weare.timeZone"))));
     }
 
     private Profile profile;
@@ -32,22 +62,16 @@ public class User {
     private String email;
     private String password;
     private ProfessionalCategory category;
-    private Date registrationDate;
+    private LocalDate registrationDate;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User u = (User) o;
 
-        return username.equals(u.username)
-                && email.equals(u.email)
-                && password.equals(u.password)
-                && category.equals(u.category)
-                && registrationDate.equals(u.registrationDate)
-                && profile.equals(u.profile);
-    }
-
+    /**
+     * Retrieves the user ID by searching the database for the user's username.
+     * Connects to the database using UserHelper.
+     *
+     * @return the user ID if found, or -1 if not found
+     * @see UserHelper
+     */
     public int getUserId() {
         return UserHelper.getUserId(UserHelper.getUser("username", String.format("'%s'", this.username)));
     }
